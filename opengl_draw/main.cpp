1369f7c347be41d948353833ebce193c1859e1b9
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-24 12:04:27 +0800

    调整窗口大小便于印刷

--- opengl_draw/main.cpp
+++ opengl_draw/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char ** argv) {
     sstd::QtApplication varApplication{ argc,argv };
 
     sstd::Window window;
-    window.resize(512, 512);
+    window.resize(512, 256);
     window.show();
 
     return varApplication.exec();
