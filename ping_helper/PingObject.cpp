fb6a49391fab42aadfc8ea9aeea09d5e4539bd6d
ngzHappy <zhaixueqiang@hotmail.com>
2019-06-23 22:38:41 +0800

    +test code

--- ping_helper/PingObject.cpp
+++ ping_helper/PingObject.cpp
@@ -1,5 +1,5 @@
 ﻿
-#include "Ping.hpp"
+//#include "Ping.hpp"
 #include "PingObject.hpp"
 
 namespace sstd{
@@ -9,27 +9,27 @@ PingObject::PingObject(QObject * argParent ){
 }
 
 class GlobalPingObject{
-    boost::asio::io_context thisContex;
-    public:
-    inline auto & getContex(){
-        return thisContex;
-    }
-    inline GlobalPingObject(){}
-    inline static auto & instance(){
-        static GlobalPingObject globalAns  ;
-        return globalAns;
-    }
+   // boost::asio::io_context thisContex;
+   // public:
+   // inline auto & getContex(){
+   //     return thisContex;
+   // }
+   // inline GlobalPingObject(){}
+   // inline static auto & instance(){
+   //     static GlobalPingObject globalAns  ;
+   //     return globalAns;
+   // }
 };
 
 void PingObject::start( const QString & arg ){
-     assert ( !thisPing );
-     thisSource = arg.toUtf8 ();
-     thisPing = new Ping(GlobalPingObject::instance ().getContex (),
-                      thisSource.constData());
+   // assert ( !thisPing );
+   // thisSource = arg.toUtf8 ();
+   // thisPing = new Ping(GlobalPingObject::instance ().getContex (),
+   //                  thisSource.constData());
 }
 
 PingObject::~PingObject(){
-    delete thisPing;
+   // delete thisPing;
 }
 
 }/*namespace sstd*/
