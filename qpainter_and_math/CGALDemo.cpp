ef10cd383582ae5f8bdb47bec0e4853a9651a935
ngzHappy <zhaixueqiang@hotmail.com>
2019-07-02 11:20:10 +0800

    +headers

--- qpainter_and_math/CGALDemo.cpp
+++ qpainter_and_math/CGALDemo.cpp
@@ -1,8 +1,19 @@
 ﻿#include "CGALDemo.hpp"
 
-namespace sstd{
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_vertex_base_with_id_2.h>
+#include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <CGAL/Polygon_2.h>
 
-    CGALDemo::CGALDemo() : SubWindowBasic (QStringLiteral("CGALDemo")) {
+namespace sstd {
+
+    CGALDemo::CGALDemo() :
+        SubWindowBasic(QStringLiteral("CGALDemo")) {
+
+        auto varScene = this->scene();
+
+
     }
 
 }/*namespace sstd*/
