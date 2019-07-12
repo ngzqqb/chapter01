cb4c14461e4abbca947d3bd37e5a80d9bcf4da63
ngzHappy <zhaixueqiang@hotmail.com>
2019-07-12 10:14:05 +0800

    +udpate

--- qpainter_and_math/BoostMultiprecisionDemo.cpp
+++ qpainter_and_math/BoostMultiprecisionDemo.cpp
@@ -2,6 +2,7 @@
 
 #include <limits>
 #include <iomanip>
+#include <sstream>
 #include <iostream>
 
 #include <sstd/boost/multiprecision/cpp_int.hpp>
@@ -9,17 +10,43 @@
 
 namespace sstd {
 
+    template<bool IsFloat = false, typename T>
+    inline static QString toQString(const T & arg) {
+        std::stringstream ss;
+        if constexpr (IsFloat) {
+            using U = std::remove_cv_t< std::remove_reference_t<T> >;
+            ss << std::setprecision(std::numeric_limits<U>::digits10) << arg;
+        } else {
+            ss << arg;
+        }
+        std::string varTmp;
+        ss >> varTmp;
+        return QString::fromLatin1(varTmp.c_str(), static_cast<int>(varTmp.size()));
+    }
+
+    template<typename T>
+    inline static auto formatText(T * arg) {
+        arg->setTextWidth(230);
+        arg->setFlag(QGraphicsItem::ItemIsMovable);
+        return arg;
+    }
+
     BoostMultiprecisionDemo::BoostMultiprecisionDemo()
         :SubWindowBasic(QStringLiteral("BoostMultiprecisionDemo")) {
 
-        using BInt = boost::multiprecision::cpp_int;
-        using BFloat = boost::multiprecision::cpp_dec_float<256>;
+        auto varScene = this->scene();
 
+        using BInt = boost::multiprecision::cpp_int;
+        using PBFloat = boost::multiprecision::cpp_dec_float<256>;
+        using BFloat = boost::multiprecision::number<PBFloat>;
+
+        qreal varHeight;
         {
-            BInt varA{ "123456789123456789123456789" };
-            BInt varB{ "789456123789456123789456123" };
-            BInt varC = varA * varB;
-            std::cout << varC << std::endl;
+            const BInt varA{ "123456789123456789123456789" };
+            const BInt varB{ "789456123789456123789456123" };
+            const BInt varC = varA * varB;
+            varHeight = formatText(varScene->addText(toQString(varC)))
+                ->boundingRect().height();
         }
 
         {
@@ -27,10 +54,8 @@ namespace sstd {
             for (auto varI = 0u; varI < 100u; ++varI) {
                 varA *= varA;
             }
-            std::cout
-                << std::setprecision(std::numeric_limits<BFloat>::digits10)
-                //<< varA
-                << std::endl;
+            formatText(varScene->addText(toQString<true>(varA)))
+                ->setY(varHeight+4.5);
         }
 
     }
