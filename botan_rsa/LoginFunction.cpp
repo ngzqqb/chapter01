712ed791ff335bda7a7b73b98126de3d3a13eba1
ngzHappy <zhaixueqiang@hotmail.com>
2019-07-13 15:14:27 +0800

    Update LoginFunction.cpp

--- botan_rsa/LoginFunction.cpp
+++ botan_rsa/LoginFunction.cpp
@@ -2,5 +2,15 @@
 
 namespace sstd{
 
+    class LoginFunction final :
+            public QObject ,
+            public sstd::YieldResumeFunction {
+        Q_OBJECT
+    public:
+        LoginFunction();
+    private:
+        sstd_class(LoginFunction);
+    };
+
 }/*namespace sstd*/
 
